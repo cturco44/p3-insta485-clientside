a29faee5bfe9a21be0d38fc98a8625f8bc42aceb
eroyama <youremail@yourdomain.com>
2020-10-03 17:53:20 -0400

    edited likes.jsx a bit

--- insta485/js/likes.jsx
+++ insta485/js/likes.jsx
@@ -9,7 +9,7 @@ class Likes extends React.Component {
   constructor(props) {
     // Initialize mutable state
     super(props);
-    this.state = { numLikes: 0, lognameLikesThis: false };
+    this.state = { numLikes: 0 };
   }
 
   componentDidMount() {
@@ -25,7 +25,6 @@ class Likes extends React.Component {
       .then((data) => {
         this.setState({
           numLikes: data.likes_count,
-          lognameLikesThis: data.logname_likes_this,
         });
       })
       .catch((error) => console.log(error));
@@ -34,7 +33,6 @@ class Likes extends React.Component {
   render() {
     // This line automatically assigns this.state.numLikes to the const variable numLikes
     const { numLikes } = this.state;
-    const { lognameLikesThis } = this.state; // use this to make the like/unlike button?
 
     // Render number of likes
     return (
