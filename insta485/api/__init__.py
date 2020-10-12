55865b24496ad4e1b34bc2fc855c87eda9cf52c2
eroyama <youremail@yourdomain.com>
2020-10-12 17:14:34 -0400

    uncommented line

--- insta485/api/__init__.py
+++ insta485/api/__init__.py
@@ -3,4 +3,4 @@
 from insta485.api.likes import get_likes
 from insta485.api.post import get_post
 from insta485.api.post_list import get_post_list
-#from insta485.api.comments import get_comments
+from insta485.api.comments import get_comments
