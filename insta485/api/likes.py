e269506382fcb82c0b4d198b49d9ee05904ae362
Connor Turco <cturco@umich.edu>
2020-10-13 00:05:27 -0400

    api done

--- insta485/api/likes.py
+++ insta485/api/likes.py
@@ -1,24 +1,51 @@
 """REST API for likes."""
 import flask
+from flask import jsonify, url_for
 import insta485
+from insta485.api.comments import InvalidUsage, handle_invalid_usage
+from insta485.api.post import checkPostid
+from insta485.views.index import num_likes, logname_liked
+from insta485.views.post import delete_post
 
 
-@insta485.app.route('/api/v1/p/<int:postid_url_slug>/likes/', methods=["GET"])
+@insta485.app.route('/api/v1/p/<int:postid_url_slug>/likes/', methods=["GET", "POST", "DELETE"])
 def get_likes(postid_url_slug):
-    """Return likes on postid.
+  """Return likes on postid"""
 
-    Example:
-    {
-      "logname_likes_this": 1,
-      "likes_count": 3,
-      "postid": 1,
-      "url": "/api/v1/p/1/likes/"
-    }
-    """
-    context = {
-        "logname_likes_this": 1,
-        "likes_count": 3,
+  if "username" in flask.session:
+        logname = flask.session["username"]
+  else:
+    raise InvalidUsage("Forbidden", status_code=403)
+
+  if not checkPostid(postid_url_slug):
+    raise InvalidUsage("Not Found", status_code=404)
+
+  if request.method == "POST":
+    if logname_liked(postid_url_slug, logname):
+      raise InvalidUsage("Conflict", status_code=409)
+      context = {
+        "logname": logname,
+        "message": "Conflict",
         "postid": postid_url_slug,
-        "url": flask.request.path,
+        "status_code": 409
+      }
+      return jsonify(**context)
+    context = {
+      "logname": logname,
+      "postid": postid_url_slug
     }
-    return flask.jsonify(**context)
+    return jsonify(**context)
+
+  if request.method == "DELETE":
+    if checkPostid(postid_url_slug):
+      delete_post(postid_url_slug)
+    return
+  
+  context = {
+    "logname_likes_this": int(logname_liked(postid_url_slug, logname)),
+    "likes_count": num_likes(postid_url_slug),
+    "postid": postid_url_slug,
+    "url": flask.request.path
+  }
+  return jsonify(**context)
+
