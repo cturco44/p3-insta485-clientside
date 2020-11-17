843a27c68a01655ec34aca69fc184a1463d9594b
eroyama <youremail@yourdomain.com>
2020-11-17 13:57:50 -0500

    works w/out aws services

--- insta485/config.py
+++ insta485/config.py
@@ -27,7 +27,7 @@ POSTGRESQL_DATABASE_DB = "insta485"
 # AWS S3 static files
 # https://flask-s3.readthedocs.io/en/latest/
 FLASKS3_DEBUG = True # Enables Flask-S3's debug mode
-FLASKS3_ACTIVE = True # This setting allows you to toggle whether Flask-S3 is active or not
+FLASKS3_ACTIVE = False # This setting allows you to toggle whether Flask-S3 is active or not
 FLASKS3_BUCKET_NAME = "eroyama.static.insta485.com" # Add your own root bucket name here, replacing uniqname with your uniqname
 FLASKS3_REGION = "us-east-2" # Sets up the AWS region to host your static assets in 
 FLASKS3_FORCE_MIMETYPE = True # Always set the Content-Type header on the S3 files irrespective of gzipping
@@ -35,6 +35,6 @@ FLASKS3_USE_HTTPS = False # We will only be using HTTP for now
 FLASKS3_CDN_DOMAIN = "http://d7ndtapyu5ir1.cloudfront.net/" # Add your own CDN Domain Name here
 
 # AWS S3 file upload
-AWS_S3_UPLOAD_BUCKET = "eroyama.uploads.insta485.com" # or your bucket name
+#AWS_S3_UPLOAD_BUCKET = "eroyama.uploads.insta485.com" # or your bucket name
 AWS_S3_UPLOAD_REGION = "us-east-2"
 AWS_S3_UPLOAD_FOLDER = "uploads"
